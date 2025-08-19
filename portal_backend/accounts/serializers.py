d4538bed065bfa1bea892bb47d403920bbe32def
Douglas Gatimu <douglasgatimu@gmail.com>
2025-08-20 01:59:06 +0300

    Add Registration and UserSerializers.

--- portal_backend/accounts/serializers.py
+++ portal_backend/accounts/serializers.py
@@ -16,3 +16,23 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    firstName = serializers.CharField(source="first_name")
+    lastName = serializers.CharField(source="last_name")
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'firstName', 'lastName']
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
