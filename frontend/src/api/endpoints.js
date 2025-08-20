f9714d2208a748e2f9a51297c1f671b5ba26df00
Douglas Gatimu <douglasgatimu@gmail.com>
2025-08-20 03:14:56 +0300

    Add register url endpoint.

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -4,3 +4,4 @@ export const COURSES_URL = `${BASE_URL}courses/`;
 export const REFRESH_URL = `${BASE_URL}api/token/refresh/`;
 export const LOGOUT_URL = `${BASE_URL}users/logout/`;
 export const IS_AUTH_URL = `${BASE_URL}users/authenticated/`;
+export const REGISTER_URL = `${BASE_URL}users/register/`;
