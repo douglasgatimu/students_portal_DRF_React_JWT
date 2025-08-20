6d9815314e012477e929052b782adff071c1a8ed
Douglas Gatimu <douglasgatimu@gmail.com>
2025-08-20 05:44:04 +0300

    Add jwt + http-only cookie, actions: me, logout, register.

--- portal_backend/accounts/views.py
+++ portal_backend/accounts/views.py
@@ -11,9 +11,17 @@ from rest_framework.authentication import TokenAuthentication
 
 from accounts.serializers import (
     UserSerializer,
+    UserRegistrationSerializer,
 
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -21,3 +29,91 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=["post"], permission_classes=[AllowAny])
+    def logout(self, request):
+        try:
+            res = Response({"success": True})
+            res.delete_cookie('access_token', path='/', samesite='None')
+            res.delete_cookie('refresh_token', path='/', samesite='None')
+            return res
+        except Exception:
+            return Response({"success": False})
+        
+    @action(detail=False, methods=["post"], permission_classes=[IsAuthenticated])
+    def authenticated(self, request):
+        return Response({'authenticated': True})
+    
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        """ current authenticated user data"""
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)    
+
+    @action(detail=False, methods=["post"], permission_classes=[AllowAny])
+    def register(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        try:
+            response = super().post(request, *args, **kwargs)
+
+            tokens = response.data
+            access_token = tokens["access"]
+            refresh_token = tokens["refresh"]
+
+            res = Response()
+
+            res.data = {'success' : True} 
+            res.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )
+            res.set_cookie(
+                key='refresh_token',
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )            
+            return res
+
+        except:
+            return Response({'success': False})
+
+class CustomRefreshTokenView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            request.data['refresh'] = refresh_token
+
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
+            access_token = tokens['access']
+
+            res = Response()
+
+            res.data = {'refreshed': True}
+
+            res.set_cookie(
+                key='access_token',
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite='None',
+                path='/'
+            )                    
+            return res
+        except:
+            return Response({'refreshed': False})
